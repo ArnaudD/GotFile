34dd17dd795e36091778d54911a9c50e4baba8e3
Arnaud Didry <arnaud@didry.info>
2010-09-28 02:16:42 +0200

    Updated Symfony2 & Documented setup command

--- src/GotFile/SetupCommand.php
+++ src/GotFile/SetupCommand.php
@@ -14,7 +14,22 @@ class SetupCommand extends Command
   protected function configure ()
   {
     $this->setName ('setup')
-         ->setDescription ('Synchronize your home with an existing repository or create a new one');
+         ->setDescription ('Synchronize your home with an existing repository or create a new one')
+         ->setDefinition(array(
+           new InputArgument('repository',    InputArgument::REQUIRED, 'URL of the git repository'),
+           new InputOption  ('home-dir', 'h', InputOption::PARAMETER_OPTIONAL, 'Home directory', '$HOME'),
+         ))
+         ->setHelp(<<<EOF
+The <info>setup</info> initialize your home directory with a git repository
+
+  <info>./gotfile setup http://github.com/ArnaudD/myDotFiles</info>
+
+You can also change the default home directory with the <comment>--home-dir</comment> option:
+
+  <info>./gotfile setup http://github.com/ArnaudD/myDotFiles --home-dir="/home/other_user"</info>
+  <info>./gotfile setup http://github.com/ArnaudD/myDotFiles -h "/home/other_user"</info>
+EOF
+    );
   }
 
   protected function execute (InputInterface $input, OutputInterface $output)
